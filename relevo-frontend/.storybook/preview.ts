0ff72c97798027feba2e431cbe4ce50b7f48feb1
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2025-07-03 15:01:56 -0300

    chore(deps): storybook bump version 9.0.5

--- relevo-frontend/.storybook/preview.ts
+++ relevo-frontend/.storybook/preview.ts
@@ -1,4 +1,3 @@
-import { withThemeByClassName } from "@storybook/addon-themes";
 import type { Preview } from "@storybook/react";
 
 /* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
@@ -15,17 +14,7 @@ const preview: Preview = {
 		},
 	},
 
-	decorators: [
-		// Adds theme switching support.
-		// NOTE: requires setting "darkMode" to "class" in your tailwind config
-		withThemeByClassName({
-			themes: {
-				light: "light",
-				dark: "dark",
-			},
-			defaultTheme: "light",
-		}),
-	],
+	decorators: [],
 };
 
 export default preview;
