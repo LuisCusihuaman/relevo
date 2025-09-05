45e77ba75b7b6033ee2195e37113f0db9101ac2e
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2025-09-05 02:24:00 -0300

    feat(routes): add Home component to authenticated route

--- relevo-frontend/src/routes/_authenticated/route.tsx
+++ relevo-frontend/src/routes/_authenticated/route.tsx
@@ -1,6 +1,8 @@
+import { Home } from "@/pages/Home";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated")({
+    component: Home,
 	async beforeLoad(context) {
 		const token = await context.context.auth?.getToken();
 		if (!token) {
